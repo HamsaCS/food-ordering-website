c0967e675e52ccae063e0809daabc6805d1e217d
Hamsa <hamsashettycs@gmail.com>
2024-02-04 21:33:17 +0530

    php has been added

---
+++ admin/manage-admin.php
@@ -0,0 +1,13 @@
+<?php include("partials/menu.php"); ?>
+    <!------main content section starts------>
+    <div class="main-content">
+    <div class="wrapper">
+        <h1>MANAGE ADMIN</h1>
+        
+
+        
+        
+        </div>
+    </div>
+    <!-------main content section ends----->
+<?php include("partials/footer.php"); ?> 
---
+++ admin/partials/footer.php
@@ -0,0 +1,11 @@
+ <!------footer section starts------>
+ <div class="footer">
+    <div class="wrapper">
+            <p class="tect-center"> 2024 all right , xoxo resturant, <a href="#">developed by xoxo team</a></p>
+        </div>
+    </div>
+    <!------footer section ends------>
+
+
+    </body>
+</html>
